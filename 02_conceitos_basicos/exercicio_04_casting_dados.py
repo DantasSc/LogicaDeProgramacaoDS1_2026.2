98b4093214a5120a09b84a6e8da2e3eabff70b73
DantasSc <dantas.emanoel12@gmail.com>
2026-09-15 21:02:14 -0300

    atividades e desafio revisadas

--- 02_conceitos_basicos/exercicio_04_casting_dados.py
+++ 02_conceitos_basicos/exercicio_04_casting_dados.py
@@ -10,7 +10,6 @@ Imprima a idade calculada com uma mensagem personalizada.
 """
 
 # TODO: Desenvolva o algoritmo abaixo:
-ano = input("Qual o ano de seu nascimento? ")
-ano_inteiro = int(ano)
+ano_inteiro = int(input("Qual o ano de seu nascimento? "))
 idade_2026 = 2026 - ano_inteiro
-print(f"No final de 2026 você terá:{idade_2026} Anos")
+print(f"No final de 2026 você terá: {idade_2026} Anos")
