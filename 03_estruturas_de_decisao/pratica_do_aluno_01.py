45d1e7092de9a8b1daf0525843286aa9f46ec866
DantasSc <dantas.emanoel12@gmail.com>
2026-09-17 13:56:24 -0300

    pratica do aluno 01

---
+++ 03_estruturas_de_decisao/pratica_do_aluno_01.py
@@ -0,0 +1,9 @@
+# TODO: Implemente a expressão de validação
+media_aluno = 7.5
+frequencia_percentual = 80
+
+# Crie a variável aprovado com a expressão lógica
+if media_aluno>=6 and frequencia_percentual>=75:
+    print("O aluno está Aprovado")
+else:
+    print("O aluno está Reprovado")
