9fcfcf199627fb1c2bc10344446d9c220c2ea4fc
DantasSc <dantas.emanoel12@gmail.com>
2026-09-17 14:09:52 -0300

    Pratica do aluno 02

---
+++ 03_estruturas_de_decisao/pratica_do_aluno_02.py
@@ -0,0 +1,8 @@
+# TODO: Escreva o programa do par ou ímpar aqui
+numero = int(input("Digite um número inteiro: "))
+divisao = numero/2
+# Escreva a estrutura if / else
+if divisao%2 ==0 :
+    print("O número é par!")
+else:
+    print("O número é impar!")
